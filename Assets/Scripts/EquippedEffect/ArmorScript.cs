c8a6de545b9b670a3f8a2789b4b95f7e9387fc7d
night-aliceio <aliceionash@gmail.com>
2025-03-04 12:07:11 -0700

    equipment

--- Assets/Scripts/EquippedEffect/ArmorScript.cs
+++ Assets/Scripts/EquippedEffect/ArmorScript.cs
@@ -5,13 +5,26 @@ using CreatorKitCode;
 public class ArmorScript : EquipmentItem.EquippedEffect
 {
     public int DefenseAmount;
-     public override void Equipped(CharacterData user)
-     {
-     int MinimumStrength;
-     }
-     
+    public override void Equipped(CharacterData user)
+    {
+        int DefenseChange = 3;
+        Sprite EffectSprite;
+    }
+    public bool Use(CharacterData user)
+    {
+        //Effect Adds Defense
+        StatSystem.StatModifier modifier = new StatSystem.StatModifier();
+        modifier.ModifierMode = StatSystem.StatModifier.Mode.Absolute;
+        modifier.Stats.defense = DefenseAmount;
+
+        VFXManager.PlayVFX(VFXType.Stronger, user.transform.position);
+
+        return true;
+    }
      public override void Removed(CharacterData user)
      {
-     
-     }
+        int DefenseChange = -3;
+        Sprite EffectSprite;
+
+    }
 }
