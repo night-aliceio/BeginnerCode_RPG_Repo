409dffcd7bcec2e79188a27897aba4900ac34e97
night-aliceio <aliceionash@gmail.com>
2025-03-03 17:27:22 -0700

    custom potion

---
+++ Assets/Scripts/EquippedEffect/ArmorScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreatorKitCode;
+public class ArmorScript : EquipmentItem.EquippedEffect
+{
+    public int DefenseAmount;
+     public override void Equipped(CharacterData user)
+     {
+     int MinimumStrength;
+     }
+     
+     public override void Removed(CharacterData user)
+     {
+     
+     }
+}
